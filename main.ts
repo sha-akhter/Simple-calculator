ddd9b7eb056fbdda8265f7590d06d06fd9d6a635
sha157 <shaakhter41@gmail.com>
2024-05-15 13:49:05 -0700

    creative file

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// printing a welcome Message
+ console.log("\n\welcome to\'shaAkhter\' - CLI simple calculator\n");
+
+// Asking Questions from users through Inquirer
+
+const answer = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber"},
+    { message: "Enter second Number", type: "number", name: "secondNumber"},
+    {
+      message: "select one of the operator to perform operation",
+      type: "list",
+      name: "operator",
+      choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+  ]);
+    
+  // conditional statement 
+  if (answer.operator === "Addition") {
+      console.log(answer.firstNumber + answer.secondNumber);
+  } else if (answer.operator === "Subtraction") {
+      console.log(answer.firstNumber - answer.secondNumber);
+  } else if (answer.operator === "Multiplication") {
+      console.log(answer.firstNumber * answer.secondNumber);
+  } else if (answer.operator ===  "Division") {
+      console.log(answer.firstNumber / answer.secondNumber);
+  } else {
+      console.log("please select valid operator")
+  }
+      
